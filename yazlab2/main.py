76c30399ea6f70a64db1030cb4e5c5c881aeebbe
omer300 <73346941+omer300@users.noreply.github.com>
2022-11-07 19:15:23 +0300

    Update main.py

--- yazlab2/main.py
+++ yazlab2/main.py
@@ -1,28 +1,15 @@
 import csv
+import nltk
 from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-from nltk.tokenize import RegexpTokenizer
-## stopword ve noktalama işretleri kaldırma testi 
-example_sent = """This is a sample sentence,
-                  showing off the stop words filtration."""
-  
-stop_words = set(stopwords.words('english'))
-  
-word_tokens = word_tokenize(example_sent)
-tokenizer = RegexpTokenizer(r'\w+')
-  
-filtered_sentence = [w for w in word_tokens if not w.lower() in stop_words]
-  
-filtered_sentence = []
-  
-for w in word_tokens:
-    if w not in stop_words:
-        filtered_sentence.append(w)
+import string
 
-filtered_sentence=tokenizer.tokenize(filtered_sentence)  #filtered_sentence list tokenize object istediğinden noktalama işaretleri atılamıyor
-print(word_tokens)
-print(filtered_sentence)
+with open('bla.txt','r') as f:
+    moby_raw = f.read()
+    stop = set(stopwords.words('english'))
+    moby_tokens = nltk.word_tokenize(moby_raw)
+    text_no_stop_words_punct = [t for t in moby_tokens if t not in stop and t not in string.punctuation]
 
+    print(text_no_stop_words_punct)
 
 
 
